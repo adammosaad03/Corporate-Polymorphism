3483efd68346caac5aeb8d5747ab8d8831d31760
adammosaad03 <mosaadadam647@gmail.com>
2025-02-16 13:24:03 -0500

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CorporatePolymorphism
+{
+  // Placeholder for Employee class implementation
+  public abstract class Employee{
+    public abstract void SubmitDailyReport();
+
+    public virtual void Work(){
+      Console.WriteLine("Employee is working.");
+    }
+    public void ClockIn(){
+      Console.WriteLine("Employee clocks in.");
+    }
+  }
+}
