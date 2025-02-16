7bd74768c9d73ffa3a9a4615f855c75a32a8c05a
adammosaad03 <mosaadadam647@gmail.com>
2025-02-16 13:24:43 -0500

    Create Manager.cs

---
+++ Manager.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CorporatePolymorphism
+{
+  // Placeholder for Manager class implementation
+  public class Manager : Employee {
+    
+    public override void Work(){
+      Console.WriteLine("Manager oversees the project.");
+    }
+    public override void SubmitDailyReport(){
+      Console.WriteLine("Manager submits daily report.");
+    }
+  }
+}
