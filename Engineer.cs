7857b95cf9302e3632089afb3e54367d4557a87e
adammosaad03 <mosaadadam647@gmail.com>
2025-02-16 13:24:28 -0500

    Create Engineer.cs

---
+++ Engineer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CorporatePolymorphism
+{
+  // Placeholder for Engineer class implementation
+  public class Engineer : Employee {
+      public override void SubmitDailyReport(){
+        Console.WriteLine("Engineer submits daily report.");
+      }
+      public override void Work(){
+        Console.WriteLine("Engineer woring on a project!");
+      }
+  }
+}
