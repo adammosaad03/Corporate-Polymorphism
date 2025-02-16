537c647f6e15b8dc32315bf2164f4748dd7450fa
adammosaad03 <mosaadadam647@gmail.com>
2025-02-16 13:24:58 -0500

    Create Intern.cs

---
+++ Intern.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CorporatePolymorphism
+{
+  // Placeholder for Intern class implementation
+  public class Intern : Employee {
+    public override void Work(){
+      Console.WriteLine("Intern assists with the project;");
+    }
+    public override void SubmitDailyReport(){
+      Console.WriteLine("Intern submits daily report.");
+    }
+  }
+}
